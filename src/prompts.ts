96734733e887aa236dfd72f2f212473344cbff2e
Etienne Donneger <etienne@pinax.network>
2025-06-19 11:43:37 -0400

    Update instructions prompt

--- src/prompts.ts
+++ src/prompts.ts
@@ -19,13 +19,14 @@ In order to discover the data, the MCP provides 4 main tools:
     4. 'run_query': run a read-only SQL query using the ClickHouse query syntax.
 
 Thus, the process of answering a users question should generally follow the steps in order presented above, first discovering networks and databases, then listing tables and finding out which one are relevant, getting the schema of those table to accurately construct and run a SQL query.
-A few tips on making queries:
+A few important tips on making queries:
     - Make sure to always get the schema of table first and not guess at column names.
     - Prefer adding LIMIT clauses to queries first to only get a sample of the data and to validate that the query is correct and relevant in the user's context. Then you can retrieve the full data if needed.
     - Be wary of resource consumption for queries, avoid too much JOIN and aggregation operations unless necessary.
     - Always use backticks when adding the database name in the query (e.g. SELECT * FROM \`database\`.table ...)
     - Remember that token names and symbols are *not* identifiable information, only the addresses are. A legitimate token can be impersonated by using the same name and symbol but its address would determine the truthworthiness. Don't rely on just name and symbols for Token queries.
     - ClickHouse may present duplicate data in the answer or multiple rows for the same unique field. That is because of the MergeTree engine functionning. You can use the 'FINAL' instruction to avoid that *but* be wary of its resource cost. Queries may take a lot longer using this keyword.
+    - Avoid using functions on column names in filters as ClickHouse would not be able to take advantage of the pre-computed indexes like bloom-filters, projections and primary keys. You should aim to always use filters that are part of a index, either as primary or secondary. You can use 'SHOW CREATE TABLE' statements to identify the indexes for a given table.
 `;
 
 export default [
