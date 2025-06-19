1488a7dc95de1805a6f89c3753326435fd123164
Etienne Donneger <etienne@pinax.network>
2025-06-19 11:49:09 -0400

    Add MVs back to `list_tables`

--- src/tools.ts
+++ src/tools.ts
@@ -20,17 +20,16 @@ export default [
         name: "list_tables",
         description: "List available tables from a database",
         parameters: z.object({
-            database: z.string() // TODO: Add validation for allowed databases on ClickHouse side (user permissions)
+            database: z.string()
         }),
         execute: async (args, { reportProgress }) => {
-            // Filter out backfill tables as well (TODO: could be done with user permissions ?)
+            // Filter out backfill tables
             const query = `SELECT name, comment AS description
                 FROM system.tables
                 WHERE
                     database = ${escapeSQL(args.database).replaceAll('"', "'")}
                     AND name NOT LIKE 'backfill_%'
                     AND name NOT LIKE '.inner_%'
-                    AND name NOT LIKE '%_mv'
                     AND name NOT LIKE 'cursors';
                 `;
             return runSQLMCP(query, reportProgress);
